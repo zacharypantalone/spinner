69b34d4a3a21297cee68329f9571977d97565bba
Zachary Pantalone <zachary.pantalone@gmail.com>
2022-06-12 13:25:10 -0400

    complete

---
+++ spinner1.js
@@ -0,0 +1,54 @@
+process.stdout.write('hello from spinner1.js... \rheyyy\n');
+
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 100);
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 300);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 500);
+
+setTimeout(() => {
+  process.stdout.write('\r\\   ');
+}, 700);
+
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 900);
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 1100);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 1300);
+
+setTimeout(() => {
+  process.stdout.write('\r\\   ');
+}, 1500);
+
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 1700);
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 1900);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 2100);
+
+setTimeout(() => {
+  process.stdout.write('\r\\   ');
+}, 2300);
+
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 2500);
+
